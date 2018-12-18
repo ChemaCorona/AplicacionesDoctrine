ab5c14077868a63b0bfda7bf5dddbbf8797d727d
ChemaCorona <chemacoronavillan@gmail.com>
2018-12-18 01:14:24 -0800

    Modificado. ORM Avance.

--- cli-config.php
+++ cli-config.php
@@ -1,13 +1,16 @@
 <?php
 // cli-config.php
+// require_once '../../bootstrap.php';
 require_once 'bootstrap.php';
-
 // Any way to access the EntityManager from  your application
-$em = GetMyEntityManager();
+// $em = $this->getDoctrine()->getEntityManager();
+
+// $helperSet = new \Symfony\Component\Console\Helper\HelperSet(array(
+//    'db' => new \Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper($em->getConnection()),
+//    'em' => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($em)
+// ));
 
 $helperSet = new \Symfony\Component\Console\Helper\HelperSet(array(
-    'db' => new \Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper($em->getConnection()),
-    'em' => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($em)
+    'em' => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($entityManager)
 ));
-
-
+return $helperSet;
